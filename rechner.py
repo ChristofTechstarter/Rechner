379c1ad415ce3866ca3eb626bea83d78dde51532
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2024-12-17 14:30:45 +0100

    Update rechner.py

--- rechner.py
+++ rechner.py
@@ -1,34 +1,15 @@
 import Skripte.grundrechenarten
 import Skripte.winkelfunktionen
+import Skripte.temperaturrechner
 
 
-def celsius_to_fahrenheit(x):
-    return (x * (9 / 5)) + 32
-
-
-def fahrenheit_to_celsius(x):
-    return (x - 32) * (5 / 9)
-
-
-def calculator():
+while True:
     print("\nWillkommen bei Christof's Rechner")
     print("Bitte wählen Sie eine der folgenden Optionen aus:")
     print("(1) Grundrechenfunktionen")
     print("(2) Winkelfunktionen")
     print("(3) Temperaturrechner")
     print("(4) Programm beenden")
-
-
-def temperaturrechner():
-    print("\nTemperaturrechner")
-    print("Bitte wählen Sie eine der folgenden Optionen aus:")
-    print("(1) Grad Celsius in Grad Fahrenheit")
-    print("(2) Grad Fahrenheit in Grad Celsius")
-    print("(3) Zurück")
-
-
-while True:
-    calculator()
     try:
 
         auswahl = int(input("\nBitte wählen Sie Ihre Operation aus (1-4): "))
@@ -40,30 +21,7 @@ while True:
             Skripte.winkelfunktionen.winkelfunktion()
         # Temperaturrechner
         elif auswahl == 3:
-            while True:
-                temperaturrechner()
-                try:
-                    auswahl = int(
-                        input("\nBitte wählen Sie Ihre Operation aus (1-3): ")
-                    )
-                    if auswahl in [1, 2]:
-                        zahl1 = float(input("Geben Sie eine Zahl ein: "))
-                        if auswahl == 1:
-                            print(
-                                f"{zahl1}°C entsprechen {celsius_to_fahrenheit(zahl1)}°F"
-                            )
-                        elif auswahl == 2:
-                            print(
-                                f"{zahl1}°F entsprechen {fahrenheit_to_celsius(zahl1)}°C"
-                            )
-                    elif auswahl == 3:
-                        break
-                    else:
-                        print(
-                            f"Ungültige Auswahl. Bitte wählen Sie eine Option zwischen 1 und 3."
-                        )
-                except ValueError:
-                    print(f"Ungültige Eingabe. Bitte geben Sie eine Zahl ein.")
+            Skripte.temperaturrechner.temperraturrechnung()
         elif auswahl == 4:
             print(f"Der Rechner wird beendet. Auf Wiedersehen!")
             break
