0d1a2cd61627269e1a100d3059fd30152c045a3c
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2024-12-17 13:29:56 +0100

    Update grundrechenarten.py

--- Skripte/grundrechenarten.py
+++ Skripte/grundrechenarten.py
@@ -18,9 +18,8 @@ def potenz(x, y):
     return x**y
 
 
-
 def grundrechenarten():
-    while True:              
+    while True:
         print("\nGrundrechenfunktionen")
         print("Bitte wählen Sie eine der folgenden Optionen aus:")
         print("(1) Addieren")
@@ -30,43 +29,32 @@ def grundrechenarten():
         print("(5) Potenzen")
         print("(6) Zurück")
         try:
+            auswahl = int(input("\nBitte wählen Sie Ihre Operation aus (1-6): "))
 
-                    auswahl = int(
-                        input("\nBitte wählen Sie Ihre Operation aus (1-6): ")
-                    )
+            if auswahl in [1, 2, 3, 4, 5]:
 
-                    if auswahl in [1, 2, 3, 4, 5]:
+                zahl1 = float(input("Geben Sie die erste Zahl ein: "))
+                zahl2 = float(input("Geben Sie die zweite Zahl ein: "))
 
-                        zahl1 = float(input("Geben Sie die erste Zahl ein: "))
-                        zahl2 = float(input("Geben Sie die zweite Zahl ein: "))
-
-                        if auswahl == 1:
-                            print(f"Das Ergebnis der Addition ist: {add(zahl1, zahl2)}")
-                        elif auswahl == 2:
-                            print(
-                                f"Das Ergebnis der Subtraktion ist: {subtract(zahl1, zahl2)}"
-                            )
-                        elif auswahl == 3:
-                            print(
-                                f"Das Ergebnis der Multiplikation ist: {mult(zahl1, zahl2)}"
-                            )
-                        elif auswahl == 4:
-                            if zahl2 != 0:
-                                print(
-                                    f"Das Ergebnis der Division ist: {div(zahl1, zahl2)}"
-                                )
-                            else:
-                                print(f"Fehler: Division durch Null ist nicht erlaubt!")
-                        elif auswahl == 5:
-                            print(
-                                f"Das Ergebnis der Potenz ist: {potenz(zahl1, zahl2)}"
-                            )
-                    elif auswahl == 6:
-                        break
+                if auswahl == 1:
+                    print(f"Das Ergebnis der Addition ist: {add(zahl1, zahl2)}")
+                elif auswahl == 2:
+                    print(f"Das Ergebnis der Subtraktion ist: {subtract(zahl1, zahl2)}")
+                elif auswahl == 3:
+                    print(f"Das Ergebnis der Multiplikation ist: {mult(zahl1, zahl2)}")
+                elif auswahl == 4:
+                    if zahl2 != 0:
+                        print(f"Das Ergebnis der Division ist: {div(zahl1, zahl2)}")
                     else:
-                        print(
-                            f"Ungültige Auswahl. Bitte wählen Sie eine Option zwischen 1 und 6."
-                        )
+                        print(f"Fehler: Division durch Null ist nicht erlaubt!")
+                elif auswahl == 5:
+                    print(f"Das Ergebnis der Potenz ist: {potenz(zahl1, zahl2)}")
+            elif auswahl == 6:
+                break
+            else:
+                print(
+                    f"Ungültige Auswahl. Bitte wählen Sie eine Option zwischen 1 und 6."
+                )
 
-                except ValueError:
-                    print(f"Ungültige Eingabe. Bitte geben Sie eine Zahl ein.")
+        except ValueError:
+            print(f"Ungültige Eingabe. Bitte geben Sie eine Zahl ein.")
